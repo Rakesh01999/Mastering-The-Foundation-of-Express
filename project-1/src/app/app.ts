c97841a3ff37dec61b2045915c60a27dd5c0162d
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-16 22:36:30 +0600

    What is parsers, request and response object --aded

--- project-1/src/app/app.ts
+++ project-1/src/app/app.ts
@@ -1,10 +1,22 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 // const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// parser
+app.use(express.json());
+app.use(express.text());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Developers !')
 })
+
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
+  // res.send("got data");
+  res.json({
+    message: "successfully received data",
+  });
+});
 
 export default app;
